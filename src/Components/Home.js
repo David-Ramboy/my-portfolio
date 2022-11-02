5f35a59a5ecba0b8ed661e51a56e35ddf44be86f
David-Ramboy <davidbryanramboy@gmail.com>
2022-11-02 18:47:16 +0800

    add

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function Home(){
     return(
         <>
-        <div className="Home-page container">
-            <h1> Hello im David Bryan </h1>
-        </div>
+   
         </>
     )
 }
