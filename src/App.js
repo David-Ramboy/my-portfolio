c0195eb8dc04412051ba881d6a54d946c2b06d92
David-Ramboy <davidbryanramboy@gmail.com>
2022-11-04 17:03:34 +0800

    Change browserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import "./App.css";
 import { Route, Link, Routes } from "react-router-dom";
 
 function App() {
+
+  function triggerClick(){
+    console.log("hi");
+  }
+
   return (
     <>
 
       <div className="Navbar container">
           <ul>
-              <Link to="/my-porfolio">Home</Link>
+              <Link to="/my-porfolio" onClick={triggerClick}>Home</Link>
               <Link to="/about">About</Link>
               <Link to="/projects">Projects</Link>
               <Link to="/social">Social Media</Link>
@@ -22,12 +27,9 @@ function App() {
       
       <Routes>
         <Route path="/my-porfolio" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About/>} /> 
         <Route path="/projects" element={<Projects/>} />
         <Route path="/social" element={<Social/>} />
-
-
-
       </Routes>
     </>
   );
